ce6eed371c4c5af954e6e647ca593da05b0eafcf
atorshizi <arian.torshizi2001@gmail.com>
2022-02-03 18:38:42 -0800

    tests should work now

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -29,11 +29,11 @@ public class MarkdownParseTest {
     }
     @Test
     public void TestGetLink() throws IOException{
-        Path fileName = Path.of("C:\\Users\\ators\\Documents\\(1) My Documents\\(1) UCSD Stuff\\2ND YEAR\\CLASSES\\2-CSE15L\\markdown-parse\\test-file2.md");
+        Path fileName = Path.of("C:\\Users\\ators\\Documents\\(1) UCSD Stuff\\2ND YEAR\\CLASSES\\2-CSE15L\\markdown-parse\\test-file2.md");
 	    String contents = Files.readString(fileName);
         assertEquals(MarkdownParse.getLinks(contents), file2);
         
-        Path fileName2 = Path.of("C:\\Users\\ators\\Documents\\(1) My Documents\\(1) UCSD Stuff\\2ND YEAR\\CLASSES\\2-CSE15L\\markdown-parse\\test-file.md");
+        Path fileName2 = Path.of("C:\\Users\\ators\\Documents\\(1) UCSD Stuff\\2ND YEAR\\CLASSES\\2-CSE15L\\markdown-parse\\test-file.md");
 	    String contents2 = Files.readString(fileName2);
         assertEquals(MarkdownParse.getLinks(contents2), file1);
     }
--- test-file.md
+++ test-file.md
@@ -6,6 +6,6 @@
 [third link](www.google.com/test)
 the following is not a link...
 "[5th link (www.linklink.com)"
-[img](pic.jpg)
+
 
 extra test....
